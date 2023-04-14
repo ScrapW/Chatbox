38118b21e4b2db8826d916e5464273b4a72f274d
ScrapW <19533993+ScrapW@users.noreply.github.com>
2023-04-14 16:06:47 +0800

    Change Options UI

--- app/src/main/java/com/scrapw/chatbox/ui/ChatboxScreen.kt
+++ app/src/main/java/com/scrapw/chatbox/ui/ChatboxScreen.kt
@@ -1,23 +1,36 @@
 package com.scrapw.chatbox.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.FastForward
+import androidx.compose.material.icons.rounded.NotificationsActive
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,7 +69,8 @@ fun IpInputBox(
 fun MessageInputBox(
     chatboxViewModel: ChatboxViewModel,
     uiState: ChatboxUiState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    icon: ImageVector = Icons.Default.Add
 ) {
     Row(
         modifier = modifier,
@@ -98,20 +112,47 @@ fun MessageInputBox(
 @Composable
 fun Option(
     description: String,
+    icon: ImageVector,
     isChecked: Boolean,
-    onChange: (Boolean) -> Unit
+    onChange: (Boolean) -> Unit,
 ) {
     Row(
-        Modifier.padding(vertical = 8.dp),
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 12.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .clickable { onChange(!isChecked) },
+
+//            .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp)),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Checkbox(
-            checked = isChecked,
-            onCheckedChange = onChange,
-            modifier = Modifier.padding(end = 8.dp)
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+//                tint = if (isChecked) MaterialTheme.colors.primary else Color.Gray,
+            modifier = Modifier
+                .size(52.dp)
+                .padding(vertical = 8.dp, horizontal = 12.dp)
         )
         Text(description)
+        Spacer(Modifier.weight(1f))
+        Switch(
+//            modifier = Modifier.wrapContentWidth(Alignment.End),
+            checked = isChecked,
+            onCheckedChange = onChange
+        )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OptionPreview() {
+    val isChecked = remember { mutableStateOf(true) }
+    Option(
+        "Description",
+        Icons.Rounded.Check,
+        isChecked.value
+    ) { isChecked.value = !isChecked.value }
 }
 
 @Composable
@@ -120,19 +161,24 @@ fun OptionList(
     uiState: ChatboxUiState,
     modifier: Modifier = Modifier
 ) {
+    val optionIcons = Icons.Rounded
+
     Column(modifier) {
         Option(
             "Real-time Message",
+            optionIcons.FastForward,
             uiState.isRealtimeMsg,
             chatboxViewModel::onRealtimeMsgChanged
         )
         Option(
             "Trigger Notification SFX",
+            optionIcons.NotificationsActive,
             uiState.isTriggerSFX,
             chatboxViewModel::onTriggerSfxChanged
         )
         Option(
             "Send Message Immediately",
+            optionIcons.Send,
             uiState.isSendImmediately,
             chatboxViewModel::onSendImmediatelyChanged
         )
