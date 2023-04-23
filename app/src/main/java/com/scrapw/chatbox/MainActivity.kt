c09c255d37f15e52746181ecf590c0a57ace17d7
ScrapW <19533993+ScrapW@users.noreply.github.com>
2023-04-24 02:04:14 +0800

    Adjust UI when open IME

--- app/src/main/java/com/scrapw/chatbox/MainActivity.kt
+++ app/src/main/java/com/scrapw/chatbox/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.scrapw.chatbox
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.scrapw.chatbox.ui.theme.ChatboxTheme
 import com.scrapw.chatbox.ui.ChatScreen
+import com.scrapw.chatbox.ui.theme.ChatboxTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
         setContent {
             ChatboxTheme {
                 // A surface container using the 'background' color from the theme
