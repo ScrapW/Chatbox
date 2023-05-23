9b5bdcd07656090187e1c2b9c31a5d25cc1d41f6
ScrapW <19533993+ScrapW@users.noreply.github.com>
2023-05-23 23:42:30 +0800

    TextField UI Update

--- app/src/main/java/com/scrapw/chatbox/ui/IpInputBox.kt
+++ app/src/main/java/com/scrapw/chatbox/ui/IpInputBox.kt
@@ -1,6 +1,8 @@
 package com.scrapw.chatbox.ui
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
@@ -8,7 +10,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun IpInputBox(
@@ -25,7 +29,10 @@ fun IpInputBox(
             onValueChange = {
                 chatboxViewModel.onIpAddressChange(it)
             },
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(all = 8.dp)
+                .clip(RoundedCornerShape(8.dp)),
             placeholder = { Text("Enter your ip here") },
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Done
--- app/src/main/java/com/scrapw/chatbox/ui/MessageInputBox.kt
+++ app/src/main/java/com/scrapw/chatbox/ui/MessageInputBox.kt
@@ -1,7 +1,11 @@
 package com.scrapw.chatbox.ui
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -14,9 +18,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -28,7 +33,7 @@ fun MessageInputBox(
     icon: ImageVector = Icons.Default.Add
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier.height(72.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         TextField(
@@ -36,7 +41,12 @@ fun MessageInputBox(
             onValueChange = {
                 chatboxViewModel.onMessageTextChange(it)
             },
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(all = 8.dp)
+                .clip(RoundedCornerShape(8.dp)),
+
             placeholder = { Text("Enter your message here") },
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Send
@@ -50,18 +60,19 @@ fun MessageInputBox(
         )
 
         Button(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(vertical = 8.dp),
             onClick = {
                 addMessage(Message(chatboxViewModel.messageText.value))
                 chatboxViewModel.sendMessage()
-            },
-            modifier = Modifier.size(48.dp),
-            content = {
-                Icon(
-                    imageVector = Icons.Filled.Send,
-                    contentDescription = "Send Message",
-                    tint = Color.White
-                )
             }
-        )
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send",
+                modifier = Modifier.size(24.dp)
+            )
+        }
     }
 }
