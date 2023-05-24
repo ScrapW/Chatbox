73033fe5616a76c0f2c37db9b7122bd21b9a91b4
ScrapW <19533993+ScrapW@users.noreply.github.com>
2023-05-24 23:31:01 +0800

    Add TopAppBar

--- app/src/main/java/com/scrapw/chatbox/ui/ChatboxScreen.kt
+++ app/src/main/java/com/scrapw/chatbox/ui/ChatboxScreen.kt
@@ -1,6 +1,11 @@
 package com.scrapw.chatbox.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -8,6 +13,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 
 val conversationUiState = ConversationUiState()
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreen(
     modifier: Modifier = Modifier,
@@ -17,6 +23,13 @@ fun ChatScreen(
 ) {
     val uiState = chatboxViewModel.uiState.collectAsState().value
     Column() {
+        TopAppBar(
+            title = { Text("Chatbox") },
+            colors = TopAppBarDefaults.largeTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        )
         IpInputBox(chatboxViewModel, uiState, modifier)
         ConversationList(
             uiState = conversationUiState,
