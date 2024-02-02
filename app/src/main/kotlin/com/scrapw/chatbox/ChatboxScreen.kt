997d5d6cd2b7db8ab5242814fa3e7daabe040075
ScrapW <19533993+ScrapW@users.noreply.github.com>
2024-02-03 05:30:03 +0800

    Settings: Fullscreen

--- app/src/main/kotlin/com/scrapw/chatbox/ChatboxScreen.kt
+++ app/src/main/kotlin/com/scrapw/chatbox/ChatboxScreen.kt
@@ -1,5 +1,10 @@
 package com.scrapw.chatbox
 
+import android.os.Build
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -24,6 +30,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.alorma.compose.settings.storage.datastore.rememberPreferenceDataStoreBooleanSettingState
 import com.scrapw.chatbox.ui.ChatboxViewModel
 import com.scrapw.chatbox.ui.mainScreen.MainScreen
 import com.scrapw.chatbox.ui.settingsScreen.SettingsScreen
@@ -77,6 +84,7 @@ fun ChatboxAppBar(
     )
 }
 
+@RequiresApi(Build.VERSION_CODES.R)
 @Composable
 fun ChatboxApp(
     modifier: Modifier = Modifier,
@@ -86,12 +94,21 @@ fun ChatboxApp(
     navController: NavHostController = rememberNavController()
 
 ) {
+
+    val fullscreenState =
+        rememberPreferenceDataStoreBooleanSettingState(
+            key = "fullscreen",
+            defaultValue = false,
+            dataStore = chatboxViewModel.dataStore
+        )
+
     val backStackEntry by navController.currentBackStackEntryAsState()
 
     val currentScreen = ChatboxScreen.valueOf(
         backStackEntry?.destination?.route ?: ChatboxScreen.Main.name
     )
 
+    setFullscreen(fullscreenState.value && currentScreen == ChatboxScreen.Main)
 
     Scaffold(
         topBar = {
@@ -121,11 +138,41 @@ fun ChatboxApp(
                 )
             }
             composable(route = ChatboxScreen.Settings.name) {
-//                val context = LocalContext.current
                 SettingsScreen(
                     chatboxViewModel = chatboxViewModel
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun setFullscreen(value: Boolean) {
+    val window = LocalContext.current.getActivity()?.window ?: return
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val controller = window.insetsController ?: return
+
+        if (value) {
+            controller.hide(WindowInsets.Type.statusBars())
+            controller.hide(WindowInsets.Type.navigationBars())
+            controller.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+            controller.show(WindowInsets.Type.statusBars())
+            controller.show(WindowInsets.Type.navigationBars())
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        if (value) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        } else {
+            window.clearFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
     }
 }
--- app/src/main/kotlin/com/scrapw/chatbox/MainActivity.kt
+++ app/src/main/kotlin/com/scrapw/chatbox/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.scrapw.chatbox
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
@@ -9,6 +11,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.scrapw.chatbox.ui.theme.ChatboxTheme
+
+
+fun Context.getActivity(): ComponentActivity? = when (this) {
+    is ComponentActivity -> this
+    is ContextWrapper -> baseContext.getActivity()
+    else -> null
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
