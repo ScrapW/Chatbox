a812c0117d0dd8dee595f4fbf5b2984db71236e3
ScrapW <19533993+ScrapW@users.noreply.github.com>
2024-02-08 04:51:16 +0800

    Revert: Navigation Bar Color = Transparent

--- app/src/main/kotlin/com/scrapw/chatbox/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/scrapw/chatbox/ui/theme/Theme.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -59,7 +58,7 @@ fun ChatboxTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
+//            window.navigationBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
