8ee15f1bbbf2aa7376080b1f13ff70034a7826c8
ScrapW <19533993+ScrapW@users.noreply.github.com>
2024-02-09 07:34:30 +0800

    strings.xml

--- app/src/main/kotlin/com/scrapw/chatbox/ui/mainScreen/Conversation.kt
+++ app/src/main/kotlin/com/scrapw/chatbox/ui/mainScreen/Conversation.kt
@@ -28,12 +28,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.scrapw.chatbox.R
 import com.scrapw.chatbox.data.SettingsStates
 import com.scrapw.chatbox.ui.common.HapticConstants
 import com.scrapw.chatbox.ui.theme.ChatboxTheme
@@ -146,7 +148,7 @@ fun EmptyConversationList(modifier: Modifier = Modifier) {
             tint = MaterialTheme.colorScheme.primaryContainer
         )
         Text(
-            text = "(Empty)",
+            text = stringResource(R.string.empty_conversation),
             style = MaterialTheme.typography.titleLarge,
             fontStyle = FontStyle.Italic,
             color = MaterialTheme.colorScheme.primaryContainer
